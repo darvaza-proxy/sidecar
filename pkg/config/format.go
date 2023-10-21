df333ad51b8e865b7dfbddcdd8c7f74cf1d42714
Alejandro Mery <amery@jpi.io>
2023-10-21 18:30:13 +0000

    config: change NewDecoder to return a wrapped ErrUnknownFormat on errors
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/config/format.go
+++ pkg/config/format.go
@@ -27,7 +27,7 @@ type Encoder interface {
 
 // NewDecoderByFilename uses the file extension
 // to determine the decoder.
-func NewDecoderByFilename(filename string) (Decoder, bool) {
+func NewDecoderByFilename(filename string) (Decoder, error) {
 	ext := filepath.Ext(filename)
 	if ext != "" {
 		ext = ext[1:]
@@ -38,20 +38,28 @@ func NewDecoderByFilename(filename string) (Decoder, bool) {
 
 // NewDecoder returns the decoder associated with
 // a name or extension.
-func NewDecoder(name string) (Decoder, bool) {
+func NewDecoder(name string) (Decoder, error) {
 	var dec Decoder
+	var key string
 
 	if alias, ok := registryAlias[name]; ok {
-		name = alias
+		key = alias
+	} else {
+		key = name
 	}
 
-	if r, ok := registry[name]; ok {
+	if r, ok := registry[key]; ok {
 		if f := r.NewDecoder; f != nil {
 			dec = f()
 		}
 	}
 
-	return dec, dec != nil
+	if dec == nil {
+		err := core.Wrap(ErrUnknownFormat, name)
+		return nil, err
+	}
+
+	return dec, nil
 }
 
 // NewEncoder returns a encoder for the specified format
